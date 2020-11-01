3ea73c2ddb63e10f9389cf88c6e1ad953ed22047
nardew <28791551+nardew@users.noreply.github.com>
2020-11-01 13:01:37 +0100

    .

--- check_new_release.sh
+++ check_new_release.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Hello World $1"
+TAG_REF=$1
+
+echo "Performing checks for version $TAG_REF"
+
+# check setup.py
+setup_py_version=`grep "version=" setup.py | cut -d '"' -f2`
+if [ "$setup_py_version" != "$TAG_REF" ]; then
+    echo "ERROR: setup.py contains version $setup_py_version which differs from $TAG_REF"
+    exit 1
+fi
+
+echo "All checks passed."
+exit 0
