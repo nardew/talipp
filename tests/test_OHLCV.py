b82828772148a2e2ac2ca00abe310d4941723041
nardew <28791551+nardew@users.noreply.github.com>
2021-08-31 16:59:53 +0200

    OHLCV with time (#57)
    * OHLCV enriched with time

--- tests/test_OHLCV.py
+++ tests/test_OHLCV.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import datetime
 
 from talipp.ohlcv import OHLCV, OHLCVFactory
 
@@ -26,6 +27,19 @@ class Test(unittest.TestCase):
                              [
                                  OHLCV(1, 2, 3, 4, None),
                                  OHLCV(6, 7, 8, 9, None),
+                             ])
+
+    def test_from_matrix_w_time(self):
+        now = datetime.now()
+        values = [
+            [1, 2, 3, 4, 5, now],
+            [6, 7, 8, 9, 0, now]
+        ]
+
+        self.assertListEqual(OHLCVFactory.from_matrix(values),
+                             [
+                                 OHLCV(1, 2, 3, 4, 5, now),
+                                 OHLCV(6, 7, 8, 9, 0, now),
                              ])
 
     def test_from_matrix2_w_volume(self):
@@ -57,6 +71,23 @@ class Test(unittest.TestCase):
                                  OHLCV(6, 7, 8, 9, None),
                              ])
 
+    def test_from_matrix2_w_time(self):
+        now = datetime.now()
+        values = [
+            [1, 6],
+            [2, 7],
+            [3, 8],
+            [4, 9],
+            [5, 0],
+            [now, now]
+        ]
+
+        self.assertListEqual(OHLCVFactory.from_matrix2(values),
+                             [
+                                 OHLCV(1, 2, 3, 4, 5, now),
+                                 OHLCV(6, 7, 8, 9, 0, now),
+                             ])
+
     def test_from_dict_w_volume(self):
         values = {
             'open': [1, 6],
@@ -84,6 +115,23 @@ class Test(unittest.TestCase):
                              [
                                  OHLCV(1, 2, 3, 4, None),
                                  OHLCV(6, 7, 8, 9, None),
+                             ])
+
+    def test_from_dict_w_time(self):
+        now = datetime.now()
+        values = {
+            'open': [1, 6],
+            'high': [2, 7],
+            'low': [3, 8],
+            'close': [4, 9],
+            'volume': [5, 0],
+            'time': [now, now]
+        }
+
+        self.assertListEqual(OHLCVFactory.from_dict(values),
+                             [
+                                 OHLCV(1, 2, 3, 4, 5, now),
+                                 OHLCV(6, 7, 8, 9, 0, now),
                              ])
 
 
