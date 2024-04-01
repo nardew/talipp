ff4b08fc1ed9e73aeed10beac5e485739f2c4f42
nardew <28791551+nardew@users.noreply.github.com>
2024-04-01 21:50:02 +0200

    Fix docs generation

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -8,11 +8,11 @@ nav = mkdocs_gen_files.Nav()
 mod_symbol = '<code class="doc-symbol doc-symbol-nav doc-symbol-module"></code>'
 
 root = Path(__file__).parent.parent
-src = root / "src"
+src = root / "talipp"
 
 for path in sorted(src.rglob("*.py")):
-    module_path = path.relative_to(src).with_suffix("")
-    doc_path = path.relative_to(src).with_suffix(".md")
+    module_path = path.relative_to(root).with_suffix("")
+    doc_path = path.relative_to(root).with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
 
     parts = tuple(module_path.parts)
@@ -21,7 +21,7 @@ for path in sorted(src.rglob("*.py")):
         parts = parts[:-1]
         doc_path = doc_path.with_name("index.md")
         full_doc_path = full_doc_path.with_name("index.md")
-    elif parts[-1].startswith("_"):
+    elif parts[-1] == "__main__":
         continue
 
     nav_parts = [f"{mod_symbol} {part}" for part in parts]
@@ -31,7 +31,7 @@ for path in sorted(src.rglob("*.py")):
         ident = ".".join(parts)
         fd.write(f"::: {ident}")
 
-    mkdocs_gen_files.set_edit_path(full_doc_path, ".." / path.relative_to(root))
+    mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
 with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
