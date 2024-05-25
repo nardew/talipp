c21a5835af107234fbb3ea5291e151d81b4ac5e5
nardew <28791551+nardew@users.noreply.github.com>
2024-05-25 06:45:36 +0200

    ZigZag indicator

--- examples/indicators.py
+++ examples/indicators.py
@@ -3,7 +3,7 @@ import random
 from talipp.indicators import AccuDist, ADX, ALMA, AO, Aroon, ATR, BB, BOP, CCI, ChaikinOsc, ChandeKrollStop, CHOP, \
     CoppockCurve, DEMA, DonchianChannels, DPO, EMA, EMV, ForceIndex, HMA, IBS, Ichimoku, KAMA, KeltnerChannels, KST, KVO, \
     MACD, MassIndex, MeanDev, OBV, PivotsHL, ROC, RSI, ParabolicSAR, SFX, SMA, SMMA, SOBV, STC, StdDev, Stoch, StochRSI, \
-    SuperTrend, T3, TEMA, TRIX, TSI, TTM, UO, VTX, VWAP, VWMA, WMA, ZLEMA
+    SuperTrend, T3, TEMA, TRIX, TSI, TTM, UO, VTX, VWAP, VWMA, WMA, ZigZag, ZLEMA
 from talipp.ohlcv import OHLCVFactory
 
 if __name__ == "__main__":
@@ -71,4 +71,5 @@ if __name__ == "__main__":
     print(f'VWAP: {VWAP(ohlcv)[-1]}')
     print(f'VWMA: {VWMA(20, ohlcv)[-1]}')
     print(f'WMA: {WMA(9, close)[-1]}')
+    print(f'ZigZag: {ZigZag(0.1, 10, ohlcv)[-5:]}')
     print(f'ZLEMA: {ZLEMA(9, close)[-1]}')
