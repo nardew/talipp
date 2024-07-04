5a2cbf5936bdca67f4e878ac54dd2fa7996168fe
nardew <28791551+nardew@users.noreply.github.com>
2024-07-04 22:38:49 +0200

    Rogers-Satchell volatility indicator

--- examples/indicators.py
+++ examples/indicators.py
@@ -2,7 +2,7 @@ import random
 
 from talipp.indicators import AccuDist, ADX, ALMA, AO, Aroon, ATR, BB, BOP, CCI, ChaikinOsc, ChandeKrollStop, CHOP, \
     CoppockCurve, DEMA, DonchianChannels, DPO, EMA, EMV, ForceIndex, HMA, IBS, Ichimoku, KAMA, KeltnerChannels, KST, KVO, \
-    MACD, MassIndex, MeanDev, OBV, ROC, RSI, ParabolicSAR, SFX, SMA, SMMA, SOBV, STC, StdDev, Stoch, StochRSI, \
+    MACD, MassIndex, MeanDev, OBV, ROC, RogersSatchell, RSI, ParabolicSAR, SFX, SMA, SMMA, SOBV, STC, StdDev, Stoch, StochRSI, \
     SuperTrend, T3, TEMA, TRIX, TSI, TTM, UO, VTX, VWAP, VWMA, WMA, ZigZag, ZLEMA
 from talipp.ohlcv import OHLCVFactory
 
@@ -49,6 +49,7 @@ if __name__ == "__main__":
     print(f'MeanDev: {MeanDev(10, close)[-1]}')
     print(f'OBV: {OBV(ohlcv)[-1]}')
     print(f'ROC: {ROC(9, close)[-1]}')
+    #print(f'RogersSatchell: {RogersSatchell(9, ohlcv)[-1]}')
     print(f'RSI: {RSI(14, close)[-1]}')
     print(f"SAR: {ParabolicSAR(0.02, 0.02, 0.2, ohlcv)[-20:]}")
     print(f'SFX: {SFX(12, 12, 3, ohlcv)[-1]}')
