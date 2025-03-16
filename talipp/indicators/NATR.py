8093e35badc76b7cca2311eaef83abc1b1908a10
nardew <28791551+nardew@users.noreply.github.com>
2025-03-16 23:02:02 +0100

    Normalized Average True Range

--- talipp/indicators/NATR.py
+++ talipp/indicators/NATR.py
@@ -1,14 +1,14 @@
-from math import sqrt, log
 from typing import List, Any
 
 from talipp.indicator_util import has_valid_values
+from talipp.indicators import ATR
 from talipp.indicators.Indicator import Indicator, InputModifierType
 from talipp.input import SamplingPeriodType
 from talipp.ohlcv import OHLCV
 
 
-class RogersSatchell(Indicator):
-    """Rogers-Satchell volatility indicator.
+class NATR(Indicator):
+    """Normalized Average True Range
 
     Input type: [OHLCV][talipp.ohlcv.OHLCV]
 
@@ -27,19 +27,21 @@ class RogersSatchell(Indicator):
                  input_indicator: Indicator = None,
                  input_modifier: InputModifierType = None,
                  input_sampling: SamplingPeriodType = None):
-        super().__init__(input_modifier=input_modifier,
-                         input_sampling=input_sampling)
+        super(NATR, self).__init__(input_modifier=input_modifier,
+                                   input_sampling=input_sampling)
 
         self.period = period
+        self.atr = ATR(period)
+
+        self.add_sub_indicator(self.atr)
 
         self.initialize(input_values, input_indicator)
 
     def _calculate_new_value(self) -> Any:
-        if not has_valid_values(self.input_values, self.period):
+        if not has_valid_values(self.atr, 1):
             return None
 
-        s = 0.0
-        for ohlcv in self.input_values[-self.period:]:
-            s += log(float(ohlcv.high) / ohlcv.close) * log(float(ohlcv.high) / ohlcv.open) + log(float(ohlcv.low) / ohlcv.close) * log(float(ohlcv.low) / ohlcv.open)
+        if self.input_values[-1].close == 0:
+            return None
 
-        return sqrt(s / self.period) if s > 0 else None
+        return 100.0 * self.atr[-1] / self.input_values[-1].close
