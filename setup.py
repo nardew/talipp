7e65bddee77d39fed7ac20e6d8a64a48e007cf1d
nardew <28791551+nardew@users.noreply.github.com>
2025-07-25 23:26:16 +0200

    Support Python version up to 3.13 (#173)
    * Support python 3.13

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries",
